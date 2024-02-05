0ba0a2826e0c2468904b9bb476fb3b18f42ce35f
Chathura Ishara <112225216+ChathuraIshara@users.noreply.github.com>
2024-02-05 22:18:28 +0530

    models attributes updated2

--- Models/Unable_Date.cs
+++ Models/Unable_Date.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class Unable_Date
+    public class Unable_Date
     {
+        public int UserId { get; set; }
+        public DateOnly Date { get; set; }
+
+
+
     }
 }
