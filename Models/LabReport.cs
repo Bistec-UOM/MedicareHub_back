b553fbbb5fffe40b2d245c39fab098f7342637cf
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-02-05 17:11:26 +0530

    init lab models

---
+++ Models/LabReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class LabReport
+    {
+
+        public int Id { get; set; }
+        public string PrescriptID { get; set; } = null!;
+        public TimeOnly Time { get; set; }
+        public DateOnly Date { get; set; }
+        public string TestName { get; set; } = null!;
+        public string Status { get; set; } = null!;
+    }
+}
---
+++ Models/ReportFields.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ReportFileds
+    {
+        public int Id { get; set; }
+        public string TestName { get; set; } = null!;
+        public string Fieldname { get; set; } = null!;
+        public short Index { get; set; }
+        public float MinRef { get; set; }
+        public float MaxRef { get; set; }
+        public string Unit { get; set; } = null!;
+    }
+}
