f366eaf5c9056a857cf46170bd6e23347c4de3fe
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-02-12 16:59:32 +0530

    Adding field list with corresponding test name

--- API/Controllers/TestController.cs
+++ API/Controllers/TestController.cs
@@ -7,63 +7,61 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-        public class TestController : ControllerBase
+    public class TestController : ControllerBase
+    {
+        private readonly TestService _tmpl;
+
+        public TestController(TestService tmpl)
         {
-            private readonly TestService _tmpl;
+            _tmpl = tmpl;
+        }
 
-            public TestController(TestService tmpl)
-            {
-                _tmpl = tmpl;
-            }
+        //Get the list of all lab tests
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Test>>> GetAllTests()
+        {
+            
+            return Ok(await _tmpl.GetAllTests());
+        }
 
-            [HttpGet]
-            public async Task<ActionResult<IEnumerable<Test>>> GetAllTests()
-            {
-                return Ok(await _tmpl.GetAllTests());
-            }
-
-            [HttpGet("{id}")]
-            public async Task<ActionResult<Test>> GetTest(int id)
-            {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Test>> GetTest(int id)
+        {
                 var tst = await _tmpl.GetTest(id);
-                if (tst == null)
-                {
-                    return NotFound();
-                }
                 return Ok(tst);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Test>> AddTest(Test item)
+        {
+            await _tmpl.Addtest(item);
+            return CreatedAtAction(nameof(GetTest), new { id = item.Id }, item);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteTest(int id)
+        {
+            await _tmpl.DeleteTest(id);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditTest(int id, Test item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
             }
 
-            [HttpPost]
-            public async Task<ActionResult<Test>> AddTest(Test item)
+            try
             {
-                await _tmpl.Addtest(item);
-                return CreatedAtAction(nameof(GetTest), new { id = item.Id }, item);
+                await _tmpl.EditTest(item);
+                return Ok();
             }
-
-            [HttpDelete("{id}")]
-            public async Task<ActionResult> DeleteTest(int id)
+            catch (KeyNotFoundException)
             {
-                await _tmpl.DeleteTest(id);
-                return NoContent();
-            }
-
-            [HttpPut("{id}")]
-            public async Task<ActionResult> EditTest(int id, Test item)
-            {
-                if (id != item.Id)
-                {
-                    return BadRequest();
-                }
-
-                try
-                {
-                    await _tmpl.EditTest(item);
-                    return Ok();
-                }
-                catch (KeyNotFoundException)
-                {
-                    return NotFound();
-                }
+                return NotFound();
             }
         }
     }
+ }
--- Services/TemplateService.cs
+++ Services/TemplateService.cs
@@ -1,19 +1,23 @@
 ﻿using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
 using Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Services
 {
     public class TemplateService
     {
         private readonly IDBop<ReportFileds> _tmplt;
-        public TemplateService(IDBop<ReportFileds> tmplt) 
+        private readonly IDBop<Test> _tst;
+        public TemplateService(IDBop<ReportFileds> tmplt,IDBop<Test> tst) 
         { 
             _tmplt = tmplt;
+            _tst = tst;
         }
 
         public async Task<IEnumerable<ReportFileds>> GetAllFields()
@@ -29,6 +33,15 @@ namespace Services
         public async Task AddField(ReportFileds item)
         {
             await _tmplt.Add(item);
+        }
+
+        public async Task AddTemplate(Test item,List<ReportFileds> item2)
+        {
+            await _tst.Add(item);
+            foreach(var i in item2)
+            {
+                await _tmplt.Add(i);
+            }
         }
 
         public async Task EditField(ReportFileds item)
