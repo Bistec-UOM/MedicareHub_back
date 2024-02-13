2d5a9c75940030623eab24a411118de067ad7933
Chathuminipamodya <ies.22.chathumini@gmail.com>
2024-02-13 18:31:23 +0530

    drug crud init

---
+++ API/Controllers/DrugsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrugsController : ControllerBase
+    {
+        private readonly DrugsService _drg;
+        public DrugsController(DrugsService drg)
+        {
+            _drg = drg;
+        }
+        [HttpPost]
+        public async Task<ActionResult<Test>> AddDrug(Drug item)
+        {
+            await _drg.Addtest(item);
+            //return CreatedAtAction(nameof(GetTest), new { id = item.TestId }, item);
+        }
+    }
+}
