d2e7dd0567f89eb8d2066fb2d1d78ec21477c91f
Chathuminipamodya <ies.22.chathumini@gmail.com>
2024-02-25 14:21:49 +0530

    GET,POST,DELETE,PUT created

--- API/Controllers/DrugsController.cs
+++ API/Controllers/DrugsController.cs
@@ -17,8 +17,42 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<string>> AddDrug(Drug item)
         {
-            await _drg.Adddrug(item);
+            await _drg.AddDrug(item);
             return Ok("Success");
         }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Drug>>> GetDrugs()
+        {
+            var drugs = await _drg.GetDrugs();
+            return Ok(drugs);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<string>> DeleteDrug(int id)
+        {
+            var result = await _drg.DeleteDrug(id);
+            if (result)
+                return Ok("Drug deleted successfully.");
+            else
+                return NotFound("Drug not found.");
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Drug>> GetDrug(int id)
+        {
+            var drug = await _drg.GetDrugById(id);
+            if (drug == null)
+                return NotFound("Drug not found.");
+
+            return Ok(drug);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<string>> UpdateDrug(int id, [FromBody] Drug updatedDrug)
+        {
+            var result = await _drg.UpdateDrug(id, updatedDrug);
+            if (result)
+                return Ok("Drug updated successfully.");
+            else
+                return NotFound("Drug not found.");
+        }
     }
+
 }
