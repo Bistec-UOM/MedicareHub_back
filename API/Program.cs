5281c7bdcbe7398daa2f9c22c363c1146507d4db
Ramosh99 <einfetch@gmail.com>
2024-02-26 18:37:24 +0530

    pushed new updates

--- API/Program.cs
+++ API/Program.cs
@@ -21,10 +21,6 @@ builder.Services.AddCors(options => {
         .AllowAnyOrigin()
         );
 });
-builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
-{
-    build.WithOrigins(" *").AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
-}));
 
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
@@ -77,7 +73,6 @@ app.UseHttpsRedirection();
 
 //pass policy name for react
 app.UseCors("ReactJSDomain");
-app.UseCors("corspolicy");
 
 app.UseAuthorization();
 
