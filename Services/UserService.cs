75091ccaf06593ca3ce8126b8970a274045c30ee
Ramosh99 <einfetch@gmail.com>
2024-03-03 19:12:05 +0530

    create business logic for deletion

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,9 +1,8 @@
 ﻿using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
 using Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Services
@@ -11,11 +10,14 @@ namespace Services
     public class UserService : IUserService
     {
         private readonly IRepository<User> _Repository;
-        
-        public UserService(IRepository<User> Repository)
+        private readonly ApplicationDbContext dbContext;
+
+        public UserService(IRepository<User> Repository, ApplicationDbContext dbContext)
         {
             _Repository = Repository;
+            this.dbContext = dbContext; // Initialize dbContext
         }
+
         public async Task AddUser(User user)
         {
             await _Repository.Add(user);
@@ -23,7 +25,19 @@ namespace Services
 
         public async Task DeleteUser(int id)
         {
+            // Retrieve appointments associated with the user ID
+            var appointmentsToDelete = await dbContext.appointments.Where(a => a.DoctorId == id).ToListAsync();
+            if (appointmentsToDelete != null) // Fixed the comparison operator
+            {
+                // Delete each appointment
+                foreach (var appointment in appointmentsToDelete)
+                {
+                    dbContext.appointments.Remove(appointment);
+                }
+            }
             await _Repository.Delete(id);
+            // Save changes to persist the deletion
+            await dbContext.SaveChangesAsync();
         }
 
         public async Task<List<User>> GetAllUsers()
