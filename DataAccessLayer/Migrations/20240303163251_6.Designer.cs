bc4295246f785b6608ccbded2efd9458900772ee
Ramosh99 <einfetch@gmail.com>
2024-03-03 22:12:56 +0530

    checked alternate solution

--- DataAccessLayer/Migrations/20240303163251_6.Designer.cs
+++ DataAccessLayer/Migrations/20240303163251_6.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240303163251_6")]
+    partial class _6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -443,7 +446,7 @@ namespace DataAccessLayer.Migrations
                     b.HasOne("Models.User", "Doctor")
                         .WithMany()
                         .HasForeignKey("DoctorId")
-                        .OnDelete(DeleteBehavior.Restrict)
+                        .OnDelete(DeleteBehavior.NoAction)
                         .IsRequired();
 
                     b.HasOne("Models.Patient", "Patient")
---
+++ DataAccessLayer/Migrations/20240303164207_7.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_appointments_users_DoctorId",
+                table: "appointments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_appointments_users_DoctorId",
+                table: "appointments",
+                column: "DoctorId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_appointments_users_DoctorId",
+                table: "appointments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_appointments_users_DoctorId",
+                table: "appointments",
+                column: "DoctorId",
+                principalTable: "users",
+                principalColumn: "Id");
+        }
+    }
+}
