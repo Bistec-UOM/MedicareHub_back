a4f8367b928195c8649118611044b5a3b882d4c1
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-03-15 16:45:44 +0530

    Value controller sample accepting

---
+++ Services/LabService/ValueService.cs
@@ -0,0 +1,30 @@
+﻿using DataAccessLayer;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.LabService
+{
+    public class ValueService
+    {
+        private readonly IRepository<Test> _tst;
+        private readonly IRepository<LabReport> _rep;
+
+        public ValueService(IRepository<Test> tst,IRepository<LabReport> rep)
+        {
+            _tst = tst;
+            _rep = rep;
+        }
+
+        async public Task AcceptSample(int id)
+        {
+            var tmp= await _rep.Get(id);
+            tmp.Status = "accepted";
+            await _rep.Update(tmp);
+        }
+
+    }
+}
