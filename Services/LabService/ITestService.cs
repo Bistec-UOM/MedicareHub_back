6ce45c53ffae64f1f45a80c81f136d4e148b6e9e
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-03-17 18:53:08 +0530

    Lab folder structure changed

---
+++ Services/LabService/ITestService.cs
@@ -0,0 +1,39 @@
+﻿using Models.DTO.Lab;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.LabService
+{
+    public interface ITestService
+    {
+        public Task<IEnumerable<Test>> GetAllTests();
+
+        public Task<Test> GetTest(int id);
+
+        public Task Addtest(Test item);
+
+        public Task EditTest(Test item);
+
+        public Task DeleteTest(int id);
+
+        public Task<IEnumerable<ReportFields>> GetAllFields();
+
+        public Task<ReportFields> GetField(int id);
+
+        public Task<IEnumerable<ReportFields>> GetFieldByTest(int id);
+
+        public Task AddTemplate(TemplateObj data);
+
+        public Task EditField(ReportFields item);
+
+        public Task EditTemplate(EdittemplateObj data);
+
+        public Task UpdateFields(List<ReportFields> data);
+
+        public Task DeleteField(int id);
+    }
+}
--- Services/LabService/TestService.cs
+++ Services/LabService/TestService.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccessLayer;
 using Models;
+using Models.DTO.Lab;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +9,14 @@ using System.Threading.Tasks;
 
 namespace Services.LabService
 {
-    public class TestService
+    public class TestService : ITestService
     {
         private readonly IRepository<Test> _test;
-        public TestService(IRepository<Test> test)
+        private readonly IRepository<ReportFields> _tmplt;
+        public TestService(IRepository<Test> test, IRepository<ReportFields> tmplt)
         {
             _test = test;
+            _tmplt = tmplt;
         }
 
         public async Task<IEnumerable<Test>> GetAllTests()
@@ -42,6 +45,95 @@ namespace Services.LabService
             if (x != null)
             {
                 await _test.Delete(id);
+            }
+        }
+
+        public async Task<IEnumerable<ReportFields>> GetAllFields()
+        {
+            return await _tmplt.GetAll();
+        }
+
+        public async Task<ReportFields> GetField(int id)
+        {
+            return await _tmplt.Get(id);
+        }
+
+        public async Task<IEnumerable<ReportFields>> GetFieldByTest(int id)
+        {
+            return await _tmplt.GetByProp("TestId", id);
+        }
+
+        public async Task AddTemplate(TemplateObj data)
+        {
+            var x = new Test
+            {
+                TestName = data.TestName,
+                Abb = data.Abb,
+                Price = data.Price,
+                Provider = data.Provider,
+            };
+            await _test.Add(x);
+
+            var Id = x.Id;
+
+            foreach (var i in data.ReportFields)
+            {
+                await _tmplt.Add(new ReportFields
+                {
+                    Fieldname = i.Fieldname,
+                    Index = i.Index,
+                    MinRef = i.MinRef,
+                    MaxRef = i.MaxRef,
+                    Unit = i.Unit,
+                    TestId = Id
+                });
+            }
+        }
+
+        public async Task EditField(ReportFields item)
+        {
+            await _tmplt.Update(item);
+        }
+
+        public async Task EditTemplate(EdittemplateObj data)
+        {
+            var existingF = await _tmplt.GetByProp("TestId", data.TestId);
+
+            //delete all existing fields
+            foreach (var item in existingF)
+            {
+                await _tmplt.Delete(item.Id);
+            }
+
+            //Add all sent fields
+            foreach (var item in data.Fields)
+            {
+                await _tmplt.Add(new ReportFields
+                {
+                    Fieldname = item.Fieldname,
+                    Index = item.Index,
+                    MinRef = item.MinRef,
+                    MaxRef = item.MaxRef,
+                    Unit = item.Unit,
+                    TestId = data.TestId
+                });
+            }
+        }
+
+        public async Task UpdateFields(List<ReportFields> data)
+        {
+            foreach (var i in data)
+            {
+                await _tmplt.Update(i);
+            }
+        }
+
+        public async Task DeleteField(int id)
+        {
+            var x = await _tmplt.Get(id);
+            if (x != null)
+            {
+                await _tmplt.Delete(id);
             }
         }
     }
