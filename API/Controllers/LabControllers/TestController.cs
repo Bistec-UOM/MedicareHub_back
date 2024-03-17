10e8810f21b75c7c0a494abcadc994c6a495ca21
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-03-17 18:43:35 +0530

    Folder structure changed

--- API/Controllers/LabControllers/TestController.cs
+++ API/Controllers/LabControllers/TestController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using Models.DTO.Lab;
 using Services.LabService;
 
 namespace API.Controllers.LabControllers
@@ -10,42 +11,60 @@ namespace API.Controllers.LabControllers
     [ApiController]
     public class TestController : ControllerBase
     {
-        private readonly TestService _tmpl;
+        private readonly TestService _tst;
 
-        public TestController(TestService tmpl)
+        public TestController(TestService test)
         {
-            _tmpl = tmpl;
+            _tst = test;
         }
 
-        //Get the list of all lab tests to display in test list==================
-
+        //Get the list of all lab tests to display in test list=================
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Test>>> GetAllTests()
         {
-            return Ok(await _tmpl.GetAllTests());
+            return Ok(await _tst.GetAllTests());
         }
 
-
-
-
         //Delete a test from available test list=================================
-
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteTest(int id)
         {
-            await _tmpl.DeleteTest(id);
+            await _tst.DeleteTest(id);
             return Ok();
         }
 
-
-
         //Edit test details from popup box=======================================
-
         [HttpPut]
         public async Task<ActionResult> EditTest(Test data)
         {
-            await _tmpl.EditTest(data);
+            await _tst.EditTest(data);
             return Ok();
         }
+
+        //Get all fields of a Test====================================
+        [HttpGet("template{id}")]
+        public async Task<ActionResult<IEnumerable<ReportFields>>> GetFieldsByTest(int id)
+        {
+            return Ok(await _tst.GetFieldByTest(id));
+        }
+
+
+        //Add new test (with templates)====================================
+        [HttpPost("template")]
+        public async Task<ActionResult> Addtmp(TemplateObj item)
+        {
+            await _tst.AddTemplate(item);
+            return Ok();
+        }
+
+        //edit existing template=============================================
+        [HttpPut("template")]
+        public async Task<ActionResult> Edittmplt(EdittemplateObj data)
+        {
+            await _tst.EditTemplate(data);
+            return Ok();
+        }
+
+
     }
 }
