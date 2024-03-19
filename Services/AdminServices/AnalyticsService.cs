ec5140e124af2609a4126eefea8bcf15d7d1f547
Ramosh99 <einfetch@gmail.com>
2024-03-19 23:34:54 +0530

    completed patient analytics end point

--- Services/AdminServices/AnalyticsService.cs
+++ Services/AdminServices/AnalyticsService.cs
@@ -9,12 +9,16 @@ namespace Services.AdminServices
 
     public class AnalyticsService : IAnalyticsService
     {
-        private readonly ApplicationDbContext? _dbcontext;
+        private readonly ApplicationDbContext _dbcontext;
+        public AnalyticsService(ApplicationDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
 
         public async Task<object> GetMaleFemalePatientsCountAllDays()
         {
             var Prescriptions = await _dbcontext.prescriptions
-                .Include(p => p.Appointment)
+                .Include(ap => ap.Appointment)
                 .ThenInclude(a => a.Patient)
                 .ToListAsync();
 
@@ -24,20 +28,77 @@ namespace Services.AdminServices
             var distinctDates = Prescriptions.Select(p => p.DateTime.Date).Distinct();
             foreach (var date in distinctDates) 
             {
-                var maleCount = Prescriptions
+                var child_male = Prescriptions
                    .Where(p => p.DateTime.Date == date)
                    .Select(p => p.Appointment)
-                   .Count(a => a?.Patient?.Gender == "Male");
-
-                var femaleCount = Prescriptions
+                   .Count(a => a?.Patient?.Gender == "Male" && CalculateAge(a.Patient.DOB) < 18);
+                var child_female = Prescriptions
                     .Where(p => p.DateTime.Date == date)
                     .Select(p => p.Appointment)
-                    .Count(a => a?.Patient?.Gender == "Female");
-                countsByDay.Add(new {Date=date,MaleCount=maleCount, FemaleCount=femaleCount});
+                    .Count(a => a?.Patient?.Gender == "Female" && CalculateAge(a.Patient.DOB) < 18);
+
+                var adult_male = Prescriptions
+                    .Where(p => p.DateTime.Date == date)
+                    .Select(p => p.Appointment)
+                    .Count(a => a?.Patient?.Gender == "Male" && CalculateAge(a.Patient.DOB) >= 18 && CalculateAge(a.Patient.DOB) < 45);
+                var adult_female = Prescriptions
+                    .Where(p => p.DateTime.Date == date)
+                    .Select(p => p.Appointment)
+                    .Count(a => a?.Patient?.Gender == "Female" && CalculateAge(a.Patient.DOB) >= 18 && CalculateAge(a.Patient.DOB) < 45);
+
+                var old_male = Prescriptions
+                    .Where(p => p.DateTime.Date == date)
+                    .Select(p => p.Appointment)
+                    .Count(a => a?.Patient?.Gender == "Male" && CalculateAge(a.Patient.DOB) > 45);
+                var old_female = Prescriptions
+                    .Where(p => p.DateTime.Date == date)
+                    .Select(p => p.Appointment)
+                    .Count(a => a?.Patient?.Gender == "Female" && CalculateAge(a.Patient.DOB) > 45);
+
+
+                //remove this after completion of this part
+                var Name = Prescriptions
+                    .Where(p => p.DateTime.Date == date)
+                    .Select(p => p.Appointment)
+                    .Select(p => p.Patient)
+                    .Select(p => new
+                    {
+                        name = p.Name,
+                        dob = p.DOB
+                    });
+                countsByDay.Add(new {
+                    Date=date,
+                    child_male=child_male, 
+                    child_female=child_female, 
+                    adult_male = adult_male, 
+                    adult_female = adult_female, 
+                    old_male = old_male, 
+                    old_female = old_female, 
+                    Name =Name
+                });
             }
 
             return countsByDay;
         }
+        private int CalculateAge(DateTime? dob)
+        {   //hasvalue use for not nullable values :- datetime?
+            if (dob.HasValue)
+            {
+                DateTime now = DateTime.Today;
+                int age = now.Year - dob.Value.Year;
+
+                // If the current date is before the birth date of this year's birthday, decrement the age
+                // This adjustment is necessary because if the birthday hasn't occurred yet this year,
+                // the age should be one less than the difference in years between birth and current date
+                if (now < dob.Value.AddYears(age))
+                {
+                    age--;
+                }
+                return age;
+            }
+            return 0; // Return 0 if date of birth is not valid
+        }
+
     }
 
 }
