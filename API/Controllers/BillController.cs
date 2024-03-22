0cada1187011dcd5d4aed17b17debd76e9fc10c6
Chathuminipamodya <ies.22.chathumini@gmail.com>
2024-03-22 23:37:42 +0530

    bill service init

---
+++ API/Controllers/BillController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BillController : ControllerBase
+    {
+        private readonly BillService _billService;
+        public BillController(BillService billService)
+        {
+            _billService = billService;
+        }
+
+
+        [HttpGet("DrugRequest")]
+        public async Task<ActionResult<IEnumerable<object>>> GetPatientPrescriptionData()
+        {
+            var pe = await _billService.RequestList();
+            return Ok(pe);
+        }
+
+
+    }
+}
---
+++ Services/BillService.cs
@@ -0,0 +1,54 @@
+﻿using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BillService
+    {
+        private readonly ApplicationDbContext _cntx;
+        public BillService(ApplicationDbContext cntx)
+        {
+            _cntx = cntx;
+        }
+
+        public async Task<IEnumerable<object>> RequestList()
+        {
+            var prescriptions = await _cntx.prescriptions.ToListAsync();
+
+            var prescriptionData = prescriptions
+                .Select(p => new
+                {
+                    Prescription = p,
+                    Drugs = _cntx.prescript_Drugs
+                        .Where(d => d.PrescriptionId == p.Id)
+                        .Select(d => new
+                        {
+                            DrugId = d.Id,
+                            GenericName = d.GenericN,
+                            Weight = d.Weight,
+                            Period = d.Period
+                        }).ToList()
+                });
+
+            var formattedData = prescriptionData.Select(p => new
+            {
+                PrescriptionId = p.Prescription.Id,
+                DateTime = p.Prescription.DateTime,
+                AppointmentId = p.Prescription.AppointmentID,
+                Total = p.Prescription.Total,
+                CashierId = p.Prescription.CashierId,
+                Drugs = p.Drugs
+            });
+
+            return formattedData;
+        }
+
+
+    }
+}
