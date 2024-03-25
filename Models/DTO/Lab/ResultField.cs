462b9e974532ccf252fefff9fd22111968409e36
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-03-25 22:51:45 +0530

    Accept incoming uploaded result

---
+++ Models/DTO/Lab/ResultField.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DTO.Lab
+{
+    public class ResultField
+    {
+        public int Fieldid { get; set; }
+        public decimal Result {  get; set; }
+        public string Status {  get; set; }
+
+    }
+}
