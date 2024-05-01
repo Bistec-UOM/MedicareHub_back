8c0bff5b09597c48411aa6618387ac3b651e03b1
Ramosh99 <einfetch@gmail.com>
2024-05-01 21:59:59 +0530

    send update for fit repo

---
+++ DataAccessLayer/Migrations/20240501162909_21.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class _21 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DoctorStatus",
+                table: "labReports");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DoctorStatus",
+                table: "labReports",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
